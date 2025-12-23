c516358e6b0bb4ee86393243a2564172a1b67c84
rasheedahmedjob <rasheedahmedjob@gmail.com>
2025-12-23 06:08:35 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  const navLinks = document.getElementById('navLinks');
+  navLinks.classList.toggle('active');
+}
+        function toggleMenu() {
+            const navLinks = document.getElementById('navLinks');
+            navLinks.classList.toggle('active');
+        }
